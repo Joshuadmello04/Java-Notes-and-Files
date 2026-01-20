78f182d739d5bdb3da7ddfe0dc3aae578b5aa2fd
Joshua Dmello <joshuadmello777@gmail.com>
2026-01-20 15:58:03 +0530

    packages lecture

---
+++ L12/Demo.java
@@ -0,0 +1,7 @@
+import com.abc.xyz.PackageDemo;
+public class Demo {
+    public static void main(String[] args) {
+        PackageDemo pd = new PackageDemo();
+        pd.abc();
+    }
+}
---
+++ L12/PackageDemo.java
@@ -0,0 +1,12 @@
+package com.abc.xyz;
+public class PackageDemo
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello from Package Demo");
+	}
+	public void abc()
+	{
+		System.out.println("Reusing the method abc()");
+	}
+}
